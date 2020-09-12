9640c4483d3b7f0ee53bc33a5e90ec3fda49c2ed
koshaapatel <koshapatel1045@gmail.com>
2020-09-12 16:01:26 -0400

    API is integrated and functionality to display conversion rate is achieved.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,35 +2,63 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="container">
-          <form class="mt-5">
-            
-              <div class="form-group">
-                <select id="currencychoice1" name="currencychoice1" class="form-control form-control-lg" v-model="currencychoice1">
-                  <option v-for="c1 in currencychoice" :key="c1" required>{{ c1 }}</option>
-                </select>
-              </div>
+      <form class="mt-5">
+        <div class="form-group">
+          <select
+            id="currencychoice1"
+            name="currencychoice1"
+            class="form-control form-control-lg"
+            v-model="currencychoice1"
+          >
+            <option v-for="c1 in currencychoice" :key="c1" required>{{ c1 }}</option>
+          </select>
+        </div>
 
-              <div class="form-group">
-                <select id="currencychoice2" name="currencychoice2" class="form-control form-control-lg" v-model="currencychoice2">
-                  <option v-for="c2 in currencychoice" :key="c2" required>{{ c2 }}</option>
-                </select>
-              </div>
+        <div class="form-group">
+          <select
+            id="currencychoice2"
+            name="currencychoice2"
+            class="form-control form-control-lg"
+            v-model="currencychoice2"
+          >
+            <option v-for="c2 in currencychoice" :key="c2" required>{{ c2 }}</option>
+          </select>
+        </div>
 
-              <div class="form-group">
-                <input type="date" v-model="date" class="form-control form-control-lg" id="date" />
-              </div>
+        <div class="form-group">
+          <input type="date" v-model="date" class="form-control form-control-lg" id="date" />
+        </div>
 
-              <div class="form-group">
-                <input type="number" v-model="number" class="form-control form-control-lg" id="number" />
-              </div>
+        <div class="form-group">
+          <input
+            type="number"
+            v-model="number"
+            class="form-control form-control-lg"
+            id="number"
+            min="1"
+          />
+        </div>
 
-              <button type="submit" @click.prevent="process" class="btn btn-primary">Submit</button>
-          </form>
-  </div>
+        <button type="submit" @click.prevent="process" class="btn btn-primary">Submit</button>
+      </form>
+      <!-- <h2 v-show="submitted" v-cloak class="mt-5">Thanks for signing up!</h2> -->
+      <div class="container" v-cloak v-show="submitted">
+        <h2 class="mt-5">To : {{ currencychoice1 }}</h2>
+        <h2 class="mt-5">From : {{ currencychoice2 }}</h2>
+        <h2 class="mt-5">Amount : {{ number }}</h2>
+        <h2 class="mt-5">Conversion Rate : {{ conversionRate}}</h2>
+        <h2 class="mt-5">Converted Amount : {{ amount}}</h2>
+      </div>
+      <!-- <div class="container" v-cloak v-show="!submitted">
+        <h2 class="mt-5">We can't process your request.</h2>
+      </div>-->
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HelloWorld",
   props: {
@@ -38,18 +66,65 @@ export default {
   },
   data: function() {
     return {
-      currencychoice: ["USD", "EUR", "JPY", "GBP", "AUD", "CHF", "CNY", "HKD", "MXN", "INR"],
-      currencychoice1: '',
-      currencychoice2: '',
-      date:'',
-      number: ''
-      
-    }
-  }, 
+      submitted: false,
+      currencychoice: [
+        "USD",
+        "EUR",
+        "JPY",
+        "GBP",
+        "AUD",
+        "CHF",
+        "CNY",
+        "HKD",
+        "MXN",
+        "INR",
+        "CAD"
+      ],
+      currencychoice1: "USD",
+      currencychoice2: "CAD",
+      date: "2020-09-02",
+      number: 1,
+      conversionRate: 0,
+      amount: 0
+    };
+  },
   methods: {
-    process: function () {
-        alert("Submitted " + this.currencychoice1 +" " +this.currencychoice2+" " + this.date+" "+this.number);
+    process: function() {
+      var apiKey = "FX" + this.currencychoice1 + this.currencychoice2;
+      var url = "https://www.bankofcanada.ca/valet/observations/" + apiKey;
+      axios
+        .get(url)
+        .then(res => {
+          //console.log(res);
+          //console.log(res.data);
+          // console.log(typeof res.data.observations);
+          let filteredData = res.data.observations.filter(
+            f => f.d === this.date
+          );
+          if (filteredData.length > 0) {
+            // console.log(filteredData[0][apiKey]);
+            this.conversionRate = filteredData[0][apiKey]["v"];
+            this.amount = (
+              parseFloat(this.conversionRate) * this.number
+            ).toFixed(4);
+            this.submitted = true;
+            // console.log(this.amount);
+            // console.log(this.conversionRate);
+          }
+          // else {
+          //   console.log(res.data);
+          // }
+        })
+        .catch(e => {
+          console.log("Can't process your request." + e);
+        });
     }
   }
 };
 </script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
